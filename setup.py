29fad5e3dc40bcc0b59c7ae78c5613d974dff454
Amal Francis <amalfra@gmail.com>
2016-10-07 13:37:23 +0530

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+from __future__ import absolute_import
+from __future__ import unicode_literals
 import re
+import os
+import codecs
+from setuptools import find_packages
 from setuptools import setup
 
-version = re.search(
-    '^__version__\s*=\s*"(.*)"',
-    open('src/__init__.py').read(),
-    re.M
-    ).group(1)
 
-with open("README.md", "rb") as f:
-    long_descr = f.read().decode("utf-8")
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding='utf-8') as fobj:
+        return fobj.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+install_requires = [
+    'requests',
+    'tabulate',
+    'python-dateutil'
+]
+
+with open('README.md', 'rb') as f:
+    long_descr = f.read().decode('utf-8')
 
 setup(
-    name="docker-hub",
-    packages=["src", "requests", "tabulate", "python-dateutil"],
-    entry_points={
-        "console_scripts": ['docker-hub=src.__main__:main']
-    },
-    version=version,
-    description="Access docker hub from your terminal",
+    name='docker-hub',
+    version=find_version('src', '__init__.py'),
+    description='Access docker hub from your terminal',
     long_description=long_descr,
-    author="Amal Francis",
-    author_email="amalfra@gmail.com",
-    url="http://github.com/amalfra/docker-hub",
+    author='Amal Francis',
+    author_email='amalfra@gmail.com',
+    url='http://github.com/amalfra/docker-hub',
+    license='MIT',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['docker-hub=src.__main__:main']
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ]
 )
