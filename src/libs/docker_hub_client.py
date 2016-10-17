34e40faefb3742178522d3a381e35f35bf8fec24
Amal Francis <amalfra@gmail.com>
2016-10-17 18:06:06 +0530

    Clean docker hub client library

--- src/libs/docker_hub_client.py
+++ src/libs/docker_hub_client.py
@@ -11,30 +11,28 @@ class DockerHubClient:
         self.config = Config()
         self.auth_token = self.config.get('auth_token')
 
-    def do_get(self, url):
+    def do_request(self, url, method='GET', data={}):
+        valid_methods = ['GET', 'POST']
+        if method not in valid_methods:
+            raise ValueError('Invalid HTTP request method')
         headers = {'Content-type': 'application/json'}
         if self.auth_token:
             headers['Authorization'] = 'JWT ' + self.auth_token
-        resp = requests.get(url, headers=headers)
-        content = {}
-        if resp.status_code == 200:
-            content = json.loads(resp.content.decode())
-        return {'content': content, 'code': resp.status_code}
-
-    def do_post(self, url, data={}):
-        data = json.dumps(data)
-        headers = {'Content-type': 'application/json'}
-        if self.auth_token:
-            headers['Authorization'] = 'JWT ' + self.auth_token
-        resp = requests.post(url, data, headers=headers)
+        request_method = getattr(requests, method.lower())
+        if len(data) > 0:
+            data = json.dumps(data)
+            resp = request_method(url, data, headers=headers)
+        else:
+            resp = request_method(url, headers=headers)
         content = {}
         if resp.status_code == 200:
             content = json.loads(resp.content.decode())
         return {'content': content, 'code': resp.status_code}
 
     def login(self, username=None, password=None):
-        resp = self.do_post(DOCKER_HUB_API_ENDPOINT + 'users/login/',
-                            {'username': username, 'password': password})
+        data = {'username': username, 'password': password}
+        resp = self.do_request(DOCKER_HUB_API_ENDPOINT + 'users/login/',
+                               'POST', data)
         if resp['code'] == 200:
             self.auth_token = resp['content']['token']
             self.config.set('auth_token', self.auth_token)
@@ -44,6 +42,6 @@ class DockerHubClient:
         return self.auth_token
 
     def get_repos(self, org, page=1):
-        url = DOCKER_HUB_API_ENDPOINT + 'repositories/' + org + '/?page=' \
-         + str(page) + '&page_size=' + str(PER_PAGE)
-        return self.do_get(url)
+        url = '{0}repositories/{1}/?page={2}&page_size={3}'. \
+               format(DOCKER_HUB_API_ENDPOINT, org, page, PER_PAGE)
+        return self.do_request(url)
