e84b9f46bb0b471ae80196d4913ee5a2aa4f59d1
Amal Francis <amalfra@gmail.com>
2022-06-25 18:15:37 +0530

    fix users command json format borken

--- src/commands/users.py
+++ src/commands/users.py
@@ -10,15 +10,20 @@ def run(docker_hub_client, args):
     """ The command to user profile from docker hub """
     resp = docker_hub_client.get_users(args.username)
     if resp['code'] == 200:
-        rows = []
+        rows = [[]] if args.format == 'json' else []
+        header = []
         for key in resp['content']:
             if key == 'date_joined':
                 formatted_date = dateutil.parser \
                                   .parse(resp['content'][key])
                 formatted_date = formatted_date.strftime("%Y-%m-%d %H:%M")
                 resp['content'][key] = formatted_date
-            rows.append([key, resp['content'][key]])
+            if args.format == 'json':
+                rows[0].append(resp['content'][key])
+                header.append(key)
+            else:
+                rows.append([key, resp['content'][key]])
         heading = f'User profile of {args.username}'
-        print_result(args.format, rows, heading=heading, count=1)
+        print_result(args.format, rows, header, heading=heading, count=1)
     else:
         print('Error fetching profile for: ' + args.username)
